31a4c8167c1ecc2d9766abed55b8815f1e6a0e63
StephenAraka <stevenaraka@gmail.com>
2026-04-08 17:53:22 +0300

    chore(client) configure text styles and fonts

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -3,6 +3,10 @@ const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["var(--font-poppins)", "sans-serif"],
+        futura: ["Futura", "var(--font-poppins)", "sans-serif"],
+      },
       colors: {
         bg: "#FCFCFC",
         primaryYellow: "#E8B40A",
@@ -11,6 +15,14 @@ const config = {
         textGreen: "#009438",
         borderGray: "#00000029",
         textBlack: "#272935",
+      },
+      fontSize: {
+        "cs-11": "11px",
+        "cs-13": "13px",
+        "cs-15": "15px",
+        "cs-16": "16px",
+        "cs-20": "20px",
+        "cs-37": "37px",
       },
     },
   },
